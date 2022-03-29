46c242157aae439abdf4e3cf55ffbe62ba305f7a
Ethan Drake <ethanjanadrake@gmail.com>
2022-03-28 18:26:18 -0700

    metas

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
-import Nav from '../components/Nav';
+import Nav from "../components/Nav";
 
 export default function Home() {
 	return (
 		<div>
 			<Head>
 				<title>FFXIV Viewer</title>
+				<meta name='charset' content='UTF-8' />
+				<meta property='og:title' content='ffxiv-viewer' />
+				<meta
+					property='og:description'
+					content='User interface for looking up character and free company info from Square Enix MMO Final Fantasy XIV'
+				/>
+				<meta property='og:image' content='/socialimage.jpg' />
+				<meta
+					name='keywords'
+					content='mmo, mmorpg, rpg, game, video, endwalker, final, fantasy'
+				/>
+				<meta name='author' content='Ethan Jana Drake' />
 			</Head>
 			<Nav linkList={[]} current={true} />
 			<h1 className='text-center pt-20'>FFXIV Viewer</h1>
